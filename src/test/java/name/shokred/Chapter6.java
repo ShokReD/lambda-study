d3db8e82a85ccd1cb2482b2be3623557534d12ea
shokred <shokred@yandex.ru>
2019-12-05 08:41:08 +0700

    Chapter 6 task 2

--- src/test/java/name/shokred/Chapter6.java
+++ src/test/java/name/shokred/Chapter6.java
@@ -2,6 +2,9 @@ package name.shokred;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Chapter6 {
@@ -10,6 +13,16 @@ public class Chapter6 {
     public void task1() {
         Assert.assertEquals(30, sequentialSumOfSquares(IntStream.range(1, 5)));
         Assert.assertEquals(30, parallelSumOfSquares(IntStream.range(1, 5)));
+    }
+
+    @Test
+    public void task2() {
+        Assert.assertEquals(600, multiplyThrough(IntStream.range(1, 6).boxed().collect(Collectors.toList())));
+    }
+
+    private static int multiplyThrough(List<Integer> linkedListOfNumbers) {
+        return linkedListOfNumbers.parallelStream()
+                .reduce(1, (acc, x) -> x * acc) * 5;
     }
 
     private static int sequentialSumOfSquares(IntStream range) {
